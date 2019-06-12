b511a28d0c56f0bdb21e20b916bd4ad78b40419d
wvit <1999wuwei@gmail.com>
2019-06-12 09:19:08 +0800

    null

--- router/blog/getBlogs.js
+++ router/blog/getBlogs.js
@@ -1,68 +1,71 @@
 const {
-    router
+  router
 } = require("../../server/server");
 const Blog = require('../../mongo/schema/blog');
 
 //后台获取博客请求
 router.get("/admin/getBlogs", async ctx => {
-    await getBlogs(ctx.request.query).then(data => {
-        ctx.body = data
-    })
+  await getBlogs(ctx.request.query).then(data => {
+    ctx.body = data
+  })
 });
 
 //前台获取博客请求
 router.get("/app/getBlogs", async ctx => {
-    await getBlogs(ctx.request.query).then(data => {
-        ctx.body = data
-    })
+  await getBlogs(ctx.request.query).then(data => {
+    ctx.body = data
+  })
 });
 
 //查询博客
 function getBlogs(reqData) {
-    const {
-        pageSize,
-        page,
-        key = '',
-        model = 1,
-        classId = '',
-    } = reqData;
-    const tags = reqData.tags ? JSON.parse(reqData.tags) : [];
-    const queryRule = {
-        isShow: true,
-        $or: [{
-            title: {
-                $regex: new RegExp(key, 'i')
-            }
-        }],
-        [Number(model) === 1 ? 'tags' : 'classId']: (() => {
-            return Number(model) === 1 ? {
-                [tags.length > 0 ? '$all' : '$ne']: tags
-            } : classId === '' ? {
-                $ne: ''
-            } : classId
-        })()
+  const {
+    pageSize,
+    page,
+    key = '',
+    model = 0, //0全部  1标签  2分类
+    classId = '', //分类的id
+  } = reqData;
+  const tags = reqData.tags ? JSON.parse(reqData.tags) : []; //标签数组
+  const queryRule = {
+    isShow: true,
+    $or: [{
+      title: {
+        $regex: new RegExp(key, 'i')
+      }
+    }]
+  };
+  const queryRuleChild = {
+    [Number(model) === 1 ? 'tags' : 'classId']: (() => {
+      return Number(model) === 1 ? {
+        [tags.length > 0 ? '$all' : '$ne']: tags
+      } : classId === '' ? {
+        $ne: ''
+      } : classId
+    })()
+  }
+  if (model) Object.assign(queryRule, queryRuleChild);
+  return new Promise(resolve => {
+    const resData = {
+      code: 0,
+      data: {
+        count: 0,
+        list: []
+      }
     };
-    return new Promise(resolve => {
-        const resData = {
-            code: 0,
-            data: {
-                count: 0,
-                list: []
-            }
-        };
-        Blog.countDocuments(queryRule)
-            .exec((err, count) => {
-                Blog.find(queryRule)
-                    .skip((Number(page) - 1) * 10)
-                    .limit(Number(pageSize))
-                    .sort({
-                        'addTime': -1
-                    })
-                    .exec((err, data) => {
-                        resData.data.count = count;
-                        resData.data.list = data;
-                        resolve(resData);
-                    })
-            })
-    })
+    Blog.countDocuments(queryRule)
+      .exec((err, count) => {
+        Blog.find(queryRule)
+          .skip((Number(page) - 1) * 10)
+          .limit(Number(pageSize))
+          .sort({
+            'addTime': -1
+          })
+          .exec((err, data) => {
+            resData.data.count = count;
+            resData.data.list = data;
+            resolve(resData);
+          })
+      })
+  })
 }
